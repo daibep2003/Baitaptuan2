530e9a649702053c7bbd4d12ea845da9c5566405
daibep2003 <tranndai01032003@gmail.com>
2023-03-17 14:10:22 +0700

    Bài Tập Tuần 2
    Bài Tập Tuần 2

---
+++ Bai2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Bai2
+{
+	public static void main(String[] args) 
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap a: ");
+		int a = sc.nextInt();
+        if(a%2 == 0)
+        {
+        	System.out.println("so vua nhap la so chan ");
+        	
+        }
+        else 
+        {
+        	System.out.println("so vua nhap la so le");
+        }
+		}
+	}
---
+++ Bai4.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class Bai4
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ban chon so (>=1 && <=12):");
+		int so = sc.nextInt();
+	if(so == 1)
+	{
+		System.out.println("Thang 1");
+	}
+	if(so == 2)
+	{
+		System.out.println("Thang 2");
+	}
+	if(so == 3)
+	{
+		System.out.println("Thang 3");
+	}
+	if(so == 4)
+	{
+		System.out.println("Thang 4");
+	}
+	if(so == 5)
+	{
+		System.out.println("Thang 5");
+	}
+	if(so == 6)
+	{
+		System.out.println("Thang 6");
+	}
+	if(so == 7)
+	{
+		System.out.println("Thang 7");
+	}
+	if(so == 8)
+	{
+		System.out.println("Thang 8");
+	}
+	if(so == 9)
+	{
+		System.out.println("Thang 9");
+	}
+	if(so == 10)
+	{
+		System.out.println("Thang 10");
+	}
+	if(so == 11)
+	{
+		System.out.println("Thang 11");
+	}
+	if(so == 12)
+	{
+		System.out.println("Thang 12");
+	}
+	}
+}
